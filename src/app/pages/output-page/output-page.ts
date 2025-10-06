571eab36c79b06c2106f2c8f822d68e513d98dab
Gurmukhtiar Singh Toor <toogurmu@sheridancollege.ca>
2025-10-06 15:40:41 -0400

    created directories

---
+++ src/app/pages/output-page/output-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output-page',
+  imports: [],
+  templateUrl: './output-page.html',
+  styleUrl: './output-page.css'
+})
+export class OutputPage {
+
+}
